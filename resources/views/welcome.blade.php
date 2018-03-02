57a3e6bc838d36e49c69bf06b947a3ee88ffe77d
Tramaine Jackson <jackson.tramaine3@gmail.com>
2018-03-02 08:53:08 -0500

    Changed some text on the reunion history

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,7 +111,7 @@
 		with both the Jackson (father’s line of decent) and Green (mother’s line of decent) families. <br/><br/>The first family reunion was held on the 
 		second weekend of August in 1982.  The reunion started with a barbeque on Friday in the backyard of Victoria's home in St. Matthew, South Carolina. 
 		On the following day, a program and dinner was held at the local middle school. Closing the family reunion weekend, on Sunday, a church services was 
-		held at Mount Salem Baptist Church in Fort Knox, South Carolina with Rev. Sandy Jackson presiding over the service.<br/><br/>Since the first reunion, 
+		held at Salem Baptist Church in Fort Motte, South Carolina with Brother Sandy Jackson presiding over the service.<br/><br/>Since the first reunion, 
 		Earlean, Victoria and Hattie Mae’s original vision to celebrate family has produced 16 more bi-annual reunions thus far.  Currently the average attendance 
 		at the Jackson-Green Family Reunions ranges between 150-200 people. Every two years the Jackson- Green reunion continues the family tradition of uplifting, 
 		celebrating, and honoring family.  The family legacy continues in 2016 as the Jackson-Green families come together in Philadelphia, PA.

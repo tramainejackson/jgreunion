18bb151c2ee6aeca271eb99688a94bc51c1044ee
Tramaine Jackson <jackson.tramaine3@gmail.com>
2018-07-20 10:13:15 -0400

    Added MDB

--- public(old)/mobile/postUpdate.php
+++
@@ -1,8 +0,0 @@
-<?php require_once("../../include/sessions.php"); ?>
-<?php require_once("../../include/court.php"); ?>
-<?php require_once("../../include/m.functions.php"); ?>
-<?php	
-	if(updatePost($_POST, $_FILES["upload_photo"]) == "Update Successful") {
-		redirect_to("mprofile.php#post");
-	}
-?>
--- public(old)/mobile/profileUpdate.php
+++
@@ -1,6 +0,0 @@
-<?php require_once("../../include/sessions.php"); ?>
-<?php require_once("../../include/court.php"); ?>
-<?php require_once("../../include/m.functions.php"); ?>
-<?php 
-	updateProfile($_POST, $_FILES);
-?>
